ed87a800e4aa6ad63f6281dc8d8e3d6877e98495
John Hugg <jhugg@voltdb.com>
2012-12-26 15:45:22 -0500

    ENG-3012: Change LICENSE.txt to MIT and update copyright to 2013. Minor formatting changes as well.

--- VoltDB.Data.Client/Connections/ProcedureWrapper[TResult,T1,...,T3].cs
+++ VoltDB.Data.Client/Connections/ProcedureWrapper[TResult,T1,...,T3].cs
@@ -1,22 +1,26 @@
-/*
+/* This file is part of VoltDB.
+ * Copyright (C) 2008-2013 VoltDB Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
 
- This file is part of VoltDB.
- Copyright (C) 2008-2011 VoltDB Inc.
-
- Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
- documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
- rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
- persons to whom the Software is furnished to do so, subject to the following conditions:
-
- The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
- Software.
-
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
- WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS BE
- LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-*/
 using System;
 using System.Threading;
 using VoltDB.Data.Client.Properties;
@@ -79,7 +83,7 @@ namespace VoltDB.Data.Client
         {
             return this.Executor.BeginExecute<TResult>(this.Callback, null, this.CommandTimeout, this.Name, this.NameUtf8Bytes, parameter1, parameter2, parameter3);
         }
-        
+
         /// <summary>
         /// Execute the procedure asynchronously and return an AsyncResponse that can be used to abort execution
         /// before it completes (using the Cancel method on the object itself).  This call will use the connection's
@@ -97,7 +101,7 @@ namespace VoltDB.Data.Client
         {
             return this.Executor.BeginExecute<TResult>(this.Callback, state, this.CommandTimeout, this.Name, this.NameUtf8Bytes, parameter1, parameter2, parameter3);
         }
-        
+
         /// <summary>
         /// Execute the procedure asynchronously and return an AsyncResponse that can be used to abort execution
         /// before it completes (using the Cancel method on the object itself).  This call will use the connection's
@@ -139,7 +143,7 @@ namespace VoltDB.Data.Client
         {
             return this.EndExecute(asyncResult as AsyncResponse<TResult>);
         }
-        
+
         /// <summary>
         /// Cancels the request associated to this execution, causing triggerring of the Asynchronous callback with a
         /// VoltClientAbortException result.  Understand that this process merely lets you decide to "forget" about
